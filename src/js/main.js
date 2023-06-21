971da8a34bec863d725dd2486b17d8a343f0e5f8
leonieschmutz <98399733+leonieschmutz@users.noreply.github.com>
2023-06-21 14:30:19 +0200

    essai draggable switch

--- src/js/main.js
+++ src/js/main.js
@@ -32,6 +32,194 @@ function initMap() {
   var options = {
     center: { lat: 46.79468556386549, lng: 7.156063246585524 },
     zoom: 15,
+    styles: [
+      [
+        {
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#212121",
+            },
+          ],
+        },
+        {
+          elementType: "labels.icon",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#757575",
+            },
+          ],
+        },
+        {
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              color: "#212121",
+            },
+          ],
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#757575",
+            },
+          ],
+        },
+        {
+          featureType: "administrative.country",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#9e9e9e",
+            },
+          ],
+        },
+        {
+          featureType: "administrative.land_parcel",
+          stylers: [
+            {
+              visibility: "off",
+            },
+          ],
+        },
+        {
+          featureType: "administrative.locality",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#bdbdbd",
+            },
+          ],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#757575",
+            },
+          ],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#181818",
+            },
+          ],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#616161",
+            },
+          ],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.stroke",
+          stylers: [
+            {
+              color: "#1b1b1b",
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#2c2c2c",
+            },
+          ],
+        },
+        {
+          featureType: "road",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#8a8a8a",
+            },
+          ],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#373737",
+            },
+          ],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#3c3c3c",
+            },
+          ],
+        },
+        {
+          featureType: "road.highway.controlled_access",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#4e4e4e",
+            },
+          ],
+        },
+        {
+          featureType: "road.local",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#616161",
+            },
+          ],
+        },
+        {
+          featureType: "transit",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#757575",
+            },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [
+            {
+              color: "#000000",
+            },
+          ],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [
+            {
+              color: "#3d3d3d",
+            },
+          ],
+        },
+      ],
+    ],
   };
   var map = new google.maps.Map(document.getElementById("map"), options);
   var marker = new google.maps.Marker({
@@ -41,4 +229,88 @@ function initMap() {
   });
 }
 
-initMap();
+// var sizeSwitch = 80;
+// var switchHandle = $("#switch .draggable-switch-handle");
+// var option1 = $("#option1");
+// var option2 = $("#option2");
+// var switchArea = $("#switch");
+
+// switchHandle.draggable({
+//   axis: "x",
+//   containment: "parent",
+//   stop: function () {
+//     conditionMove();
+//   },
+// });
+
+// option1.click(function () {
+//   option1.addClass("active");
+//   option2.removeClass("active");
+//   switchHandle.animate(
+//     {
+//       left: 0,
+//     },
+//     100
+//   );
+// });
+
+// option2.click(function () {
+//   option2.addClass("active");
+//   option1.removeClass("active");
+//   switchHandle.animate(
+//     {
+//       left: "80px" /* Résultat de $size-switch en pixels */,
+//     },
+//     100
+//   );
+// });
+
+// switchArea.click(function () {
+//   conditionMoveSnap();
+// });
+
+// function conditionMove() {
+//   if (parseInt(switchHandle.css("left")) <= 40) {
+//     /* Résultat de $size-switch / 2 en pixels */
+//     switchHandle.animate(
+//       {
+//         left: 0,
+//       },
+//       100
+//     );
+//     option1.addClass("active");
+//     option2.removeClass("active");
+//   } else {
+//     switchHandle.animate(
+//       {
+//         left: "80px" /* Résultat de $size-switch en pixels */,
+//       },
+//       100
+//     );
+//     option2.addClass("active");
+//     option1.removeClass("active");
+//   }
+// }
+
+// function conditionMoveSnap() {
+//   if (parseInt(switchHandle.css("left")) === 80) {
+//     /* Résultat de $size-switch en pixels */
+//     switchHandle.animate(
+//       {
+//         left: 0,
+//       },
+//       100
+//     );
+//     option1.addClass("active");
+//     option2.removeClass("active");
+//   } else {
+//     switchHandle.animate(
+//       {
+//         left: "80px" /* Résultat de $size-switch en pixels */,
+//       },
+//       100
+//     );
+//     option2.addClass("active");
+//     option1.removeClass("active");
+//   }
+// }
